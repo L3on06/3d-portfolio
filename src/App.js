d534b5017f2f89c298cf74045454f5bce70b16dd
L3on06 <l3onshabani@gmail.com>
2023-01-12 08:03:34 +0100

    I started calling the 3d about page.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import Spline from '@splinetool/react-spline';
 
 export default function App() {
+
   return (
     <div className='main'>
-      <Spline scene="https://prod.spline.design/dJEYQluapHYSl852/scene.splinecode" />
+      <Spline className='spline fade-in' scene="https://prod.spline.design/dJEYQluapHYSl852/scene.splinecode" />
+      <Spline className='spline fade-in' scene="https://prod.spline.design/v91dMYjNvVfWmhsD/scene.splinecode" />
     </div>
   );
 }
