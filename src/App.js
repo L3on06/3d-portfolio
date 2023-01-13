1381872ee090ba26e2aee6864a23cd27726c421c
L3on06 <l3onshabani@gmail.com>
2023-01-13 21:13:15 +0100

    I started splitting the pages.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Spline from '@splinetool/react-spline';
 
 export default function App() {
+  const [page, setPage] = useState(1);
 
   return (
     <div className='main'>
-      <Spline className='spline fade-in' scene="https://prod.spline.design/dJEYQluapHYSl852/scene.splinecode" />
-      <Spline className='spline fade-in' scene="https://prod.spline.design/v91dMYjNvVfWmhsD/scene.splinecode" />
+      {page === 1 && (
+        <Spline className='spline fade-in' scene="https://prod.spline.design/dJEYQluapHYSl852/scene.splinecode" />
+      )}
+      {page === 2 && (
+        <Spline className='spline fade-in' scene="https://prod.spline.design/v91dMYjNvVfWmhsD/scene.splinecode" />
+      )}
     </div>
   );
 }
