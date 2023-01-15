3eded327e46de475f80a27b26fdc757eaacf1f3a
L3on06 <l3onshabani@gmail.com>
2023-01-15 13:09:47 +0100

    I added the functionality to change pages by spinning the wheel.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
 import Spline from '@splinetool/react-spline';
 import Pagination from './Pagination';
@@ -6,6 +6,37 @@ import Pagination from './Pagination';
 
 export default function App() {
   const [page, setPage] = useState(1);
+  const prevPage = useRef(0);
+
+  useEffect(() => {
+    window.addEventListener("wheel", handleWheel);
+    // eslint-disable-next-line
+  }, []);
+
+  const handleWheel = (e) => {
+
+    window.removeEventListener("wheel", handleWheel);
+
+
+    if (e.deltaY < 0) {
+      // Scroll Up
+      if (prevPage.current > 0) {
+        prevPage.current -= 1;
+        setPage(prevPage.current + 1);
+      }
+    }
+    else if (e.deltaY > 0) {
+      // Scoll Down
+      if (prevPage.current < 1) {
+        prevPage.current += 1;
+        setPage(prevPage.current + 1);
+      }
+    }
+
+    setTimeout(() => {
+      window.addEventListener("wheel", handleWheel);
+    }, 2000);
+  };
 
   const handlePageChange = (index) => {
     setPage(index);
